d35853867cbe73f2300bac71be6ef7e101545302
charlene0122 <zhifeiwu@hotmail.com>
2024-01-16 13:43:51 -0800

    Adding HW1

---
+++ HW1/HelloWorld.java
@@ -0,0 +1,107 @@
+/**
+ * CIT 5910 HW1
+ * @author zhifeiwu
+ * 
+ */
+package hw1;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		//1. Print "Hello World!"
+		System.out.println("Hello World!");
+		
+		System.out.println();
+		System.out.println("----------");
+		System.out.println();
+		
+		
+		//2. Print the result of 1.22 multiplied by 49021
+		System.out.println(1.22 * 49021);
+
+		System.out.println();
+		System.out.println("----------");
+		System.out.println();
+		
+		//3. Print the result of 3 divided by 0.3
+		System.out.println(3 / 0.3);
+
+		System.out.println();
+		System.out.println("----------");
+		System.out.println();
+		
+		//4. Print the result of 214 divided by 6, discarding the remainder
+		System.out.println(214 / 6);
+		
+		System.out.println();
+		System.out.println("----------");
+		System.out.println();
+		
+		//5. Print the result of 214 divided by 6, without discarding the remainder
+		System.out.println(214.0 / 6.0);
+
+		System.out.println();
+		System.out.println("----------");
+		System.out.println();
+		
+		//6. Print the remainder of 214 divided by 6
+		System.out.println(214 % 6);
+
+		System.out.println();
+		System.out.println("----------");
+		System.out.println();
+		
+		//7. Print the value of 3 raised to the 11th power
+		System.out.println(Math.pow(3, 11));
+
+		System.out.println();
+		System.out.println("----------");
+		System.out.println();
+		
+		//8. Cast the value of 6 divided by 4 to a String and print it
+		System.out.println(Integer.toString(6 / 4));
+
+		System.out.println();
+		System.out.println("----------");
+		System.out.println();
+		
+		//9. Concatenate and print the Strings 'I', 'Love', and 'Java'. Add a single empty space
+		//between the Strings.
+		System.out.println("I " + "Love " + "Java");
+
+		System.out.println();
+		System.out.println("----------");
+		System.out.println();
+		
+		//10. Fix the following print statements so the output appears as: 4 % 2 = 0 
+		System.out.print("4 % 2 = ");
+		System.out.println(4 % 2);
+
+		System.out.println();
+		System.out.println("----------");
+		System.out.println();
+		
+		//11. Print the following sentences, (including the double quotes):
+		//Albert Einstein's best quote is "I have no special talent. I am only passionately curious."
+		System.out.println("Albert Einstein's best quote is \"I have no special talent. I am only passionately curious.\"");
+
+		System.out.println();
+		System.out.println("----------");
+		System.out.println();
+		
+		//12. Print the following sentences, (including the backslashes and double quotes):
+		//In Java Strings, the backslash (\) is a special character, also called the "escape" character.
+		//Prefixing a special character (e.g. single quote) with a backslash (\) turns it into an ordinary character.
+		System.out.println("In Java Strings, the backslash (\\) is a special character, also called the \"escape\" character.");
+		System.out.println("Prefixing a special character (e.g. single quote) with a backslash (\\) turns it into an ordinary character.");
+		
+		System.out.println();
+		System.out.println("----------");
+		System.out.println();
+		
+		//13. Print "Goodbye!"
+		System.out.println("Goodbye!");
+		 
+	}
+}
